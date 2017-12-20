7ff8f0f1b879136217936990b601084fdd7e67e7
Max Semyak <msemyak@gmail.com>
2017-12-20 03:41:08 +0200

    Some garbage removed

--- app/src/main/java/dev/msemyak/geocam/utils/CompareSizesByArea.kt
+++ app/src/main/java/dev/msemyak/geocam/utils/CompareSizesByArea.kt
@@ -7,6 +7,5 @@ import java.util.Comparator
 
 internal class CompareSizesByArea : Comparator<Size> {
 
-    override fun compare(lhs: Size, rhs: Size) =
-            signum(lhs.width.toLong() * lhs.height - rhs.width.toLong() * rhs.height)
+    override fun compare(lhs: Size, rhs: Size) = signum(lhs.width.toLong() * lhs.height - rhs.width.toLong() * rhs.height)
 }
